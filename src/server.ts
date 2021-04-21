38024695ec2f582336e57a2d9bb15ee8e25425b3
jvbs <jvbs98@hotmail.com>
2021-04-21 15:38:49 -0300

    criada primeira rota get

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,19 @@ import express from 'express'
 
 const app = express()
 
+/**
+ * GET - Buscas
+ * POST - Criação
+ * PUT - Alteração
+ * DELETAR - Deletar
+ * PATCH - Alterar uma informação específica
+ */
+
+app.get('/', (req, res) => {
+  // return res.send('Olá NLW #5')
+  return res.json({
+    message: 'ola nlw 5'
+  })
+})
+
 app.listen(3333, () => console.log('Server is running...'))
