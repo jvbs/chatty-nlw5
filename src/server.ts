1188a4084acc3fbc12ffdc6e495f80d28c67a1a2
jvbs <jvbs98@hotmail.com>
2021-04-21 20:34:00 -0300

    adc repositorios e removida node_modules

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,6 @@ const app = express()
 
 import "./database"
 
-/**
- * GET - Buscas
- * POST - Criação
- * PUT - Alteração
- * DELETAR - Deletar
- * PATCH - Alterar uma informação específica
- */
 
-app.get('/', (req, res) => {
-  // return res.send('Olá NLW #5')
-  return res.json({
-    message: 'ola nlw 5'
-  })
-})
-
-app.post('/users', (req, res) => {
-  return res.json({ message: 'Usuario salvo com sucesso!' })
-})
 
 app.listen(3333, () => console.log('Server is running...'))
